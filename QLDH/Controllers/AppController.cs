6337b58368c045dc6f1dc9492adf7f4e2b8f15bf
taquangson <quangson0409@gmail.com>
2020-04-15 12:33:41 +0700

    commit

---
+++ QLDH/Controllers/AppController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using QLDH.App_Start;
+using QLDH.DataAccess.DAO;
+using QLDH.DataAccess.Models;
+namespace QLDH.Controllers
+{
+    
+    public class AppController : ApiController
+    {
+    }
+}
